4b18275eec44f6eaec207e10bb2f557e03b7a8c0
SILVA Tiago Mendes <TMFSILVA@mc.pt>
2026-05-22 14:42:31 +0100

    sdsdsd

--- src/main/java/com/demo/api/DemoApiApplication.java
+++ src/main/java/com/demo/api/DemoApiApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApiApplication {
     public static void main(String[] args) {
-<<<<<<< HEAD
         SpringApplication.run(DemoApiApplication.class, args); 
-=======
-        SpringApplication.run(DemoApiApplication.class, args); // just a comment
->>>>>>> 10abe7258400d57a353f15765a849439a72772be
     }
 }
