6654703cf3df761eb115ee2a9880831d47651de6
SILVA Tiago Mendes <TMFSILVA@mc.pt>
2026-05-22 15:04:19 +0100

    teste2

--- src/main/java/com/demo/print.java
+++ src/main/java/com/demo/print.java
@@ -3,5 +3,8 @@ package com.demo;
 public class print {
     static void main(String[] args) {
         System.out.println("Hello, World!");
+        system.out.println("This is a test.");
     }
+
+
 }
